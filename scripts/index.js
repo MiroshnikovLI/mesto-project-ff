34fde5d5d94a1aa1a3111aaa7f090db206c0ad4b
Miroshnikov_li <MiroshnikovLI@yandex.ru>
2024-09-21 12:09:55 +0300

    fix JS Logic

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,9 @@
 // @todo: Темплейт карточки
   
   const pageContainer = document.querySelector('.page');
-  const cardTemplate = pageContainer.querySelector('#card-template').content;
   const cardsContainer = pageContainer.querySelector('.places__list');
   const buttonNewCard = pageContainer.querySelector('.profile__add-button');
-  const ButonEditProfil = pageContainer.querySelector('.profile__edit-button');
+  const butonEditProfil = pageContainer.querySelector('.profile__edit-button');
   const popuppNewCard = pageContainer.querySelector('.popup_type_new-card');
   const popupEditProfil = pageContainer.querySelector('.popup_type_edit');
   const popupImages = pageContainer.querySelector('.popup_type_image');
@@ -13,10 +12,10 @@
 // @todo: Функция открытия попап
 
   function oppenPopup(popup){
-      popup.classList.add('popup_is-opened');
+    popup.classList.add('popup_is-opened');
   
-      const buttonClosePoppap = popup.querySelector('.popup__close');
-      buttonClosePoppap.addEventListener('click', (evt) => closePopup(evt));
+    const buttonClosePoppap = popup.querySelector('.popup__close');
+    buttonClosePoppap.addEventListener('click', (evt) => closePopup(evt));
   }
 
 // @todo: Функция закрытия попап
@@ -28,7 +27,7 @@
 // @todo: Слушатели событий
 
   buttonNewCard.addEventListener('click', function(){ oppenPopup(popuppNewCard)});
-  ButonEditProfil.addEventListener('click', function(){ oppenPopup(popupEditProfil)});
+  butonEditProfil.addEventListener('click', function(){ oppenPopup(popupEditProfil)});
 
 // @todo: Функция изображения в попапе
 
@@ -40,26 +39,30 @@
 
 // @todo: DOM узлы
 
+const cardTemplate = pageContainer.querySelector('#card-template').content;
+
 // @todo: Функция создания карточки
 
-function addCards(name, link) {
-  const addCard = cardTemplate.querySelector('.card').cloneNode(true);
 
-  addCard.querySelector('.card__title').textContent = name;
-  addCard.querySelector('.card__image').src = link;
-  addCard.querySelector('.card__image').alt = name;
+function createCard(name, link, deliteCard, likeCard, popupImg) {
 
-  const LikeButtonCard = addCard.querySelector('.card__like-button');
-  LikeButtonCard.addEventListener('click', (evt) => likeCard(evt));    
-    
-  const DeleteButtonCard = addCard.querySelector('.card__delete-button');
-  DeleteButtonCard.addEventListener('click', (evt) => deliteCard(evt));
-    
-  const cardImage = addCard.querySelector('.card__image');
+  const cardsElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardsElement.querySelector('.card__image');
+  const deleteButtonCard = cardsElement.querySelector('.card__delete-button');
+  const likeButtonCard = cardsElement.querySelector('.card__like-button');
+  
+  cardsElement.querySelector('.card__title').textContent = name;
+  cardsElement.querySelector('.card__image').src = link;
+  cardsElement.querySelector('.card__image').alt = name;
+  
+  deleteButtonCard.addEventListener('click', (evt) => deliteCard(evt));
+  
+  likeButtonCard.addEventListener('click', (evt) => likeCard(evt));    
+  
   cardImage.addEventListener('click', (evt) => popupImg(evt));
   cardImage.addEventListener('click', () => oppenPopup(popupImages));
-
-  cardsContainer.append(addCard);
+  
+  return cardsElement;
 }
 
 // @todo: Функция лайка карточки
@@ -68,18 +71,17 @@ function likeCard(like) {
   like.target.classList.toggle('card__like-button_is-active');
 }
 
-
 // @todo: Функция удаления карточки
 
-function deliteCard(card) {
-  card.target.parentElement.remove();
+function deliteCard(evt) {
+  evt.target.closest('.card').remove();
 }
 
 // @todo: Вывести карточки на страницу
 
 function showCards() {
   for (let i = 0; i < initialCards.length; i++) {
-      addCards(initialCards[i].name, initialCards[i].link);
+    cardsContainer.prepend(createCard(initialCards[i].name, initialCards[i].link, deliteCard, likeCard, popupImg));
     }
 }
 
